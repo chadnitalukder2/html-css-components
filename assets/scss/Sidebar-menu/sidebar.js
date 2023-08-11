6ee54b426a620deeb1954e273dbcecb7efb2c2f0
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-08-11 23:14:13 +0600

    sidebar-2

---
+++ assets/scss/Sidebar-menu/sidebar.js
@@ -0,0 +1,12 @@
+const body = document.querySelector("body"),
+      sidebar = body.querySelector(".sidebar"),
+      toggle = body.querySelector(".toggle"),
+      serachBox = body.querySelector(".search-box"),
+      modeSwtich= body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text"),
+
+      modeSwtich.addEventlistener("click", () =>{
+        body.classList.toggle("dark");
+
+
+      });
